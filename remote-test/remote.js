d599d96e69d7dd80038f9b8d5f9e7abc335d0a74
Dr Paul Brewer <drpaulbrewer@gmail.com>
2017-04-10 01:52:43 -0400

    use fname

--- remote-test/remote.js
+++ remote-test/remote.js
@@ -33,7 +33,7 @@ function locksObtained(n, clear){
     }
     const now = Date.now();
     return (Promise
-	    .all(win.map((x,j)=>{ return vaporlock(bucket, 'lockfile').then( claim(j), ignore ); }))
+	    .all(win.map((x,j)=>{ return vaporlock(bucket, fname).then( claim(j), ignore ); }))
 	    .then(function(){
 		let i,l,sum=0;
 		for(i=0,l=win.length;i<l;++i) sum += win[i];
